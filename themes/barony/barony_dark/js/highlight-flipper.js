06329c3440458ea09a5cba7a4d8d7dacaee2948b
Dan McEwan <dan.mcewan@gmail.com>
2017-04-24 07:12:09 +0000

    drop-shadow all the things!

--- themes/barony/barony_dark/js/highlight-flipper.js
+++ themes/barony/barony_dark/js/highlight-flipper.js
@@ -1,22 +1,22 @@
 (function ($) {
   Drupal.behaviors.barony_dark_highlight_flipper = {
     attach: function (context, settings) {
-    	$(document).ready(function(){
-				if($(window).width() <= 600)
-				{
-	        $('.front #mini-panel-barony_hompage_highlights .row .panel-panel').css('display', 'block');
-				}
-			});
+      $(document).ready(function(){
+        if($(window).width() <= 600)
+        {
+          $('.front #mini-panel-barony_hompage_highlights .row .panel-panel').css('display', 'block');
+        }
+      });
 
 
-			$('.barony-homepage-highlight').hover(function(){
-				if (!$(this).hasClass('animated')) {
-					$(this).find('.barony-homepage-highlight-menu').dequeue().stop().css("display", "flex").hide().fadeIn(400, 'swing');
-				}
-			}, function() {
-		    $(this).addClass('animated').find('.barony-homepage-highlight-menu').dequeue().stop().fadeOut(400, 'swing');
-				$(this).removeClass('animated').dequeue();
-			});
+      $('.barony-homepage-highlight').hover(function(){
+        if (!$(this).hasClass('animated')) {
+          $(this).find('.barony-homepage-highlight-menu').dequeue().stop().css("display", "flex").hide().fadeIn(400, 'swing');
+        }
+      }, function() {
+        $(this).addClass('animated').find('.barony-homepage-highlight-menu').dequeue().stop().fadeOut(400, 'swing');
+        $(this).removeClass('animated').dequeue();
+      });
     }
   }
 })(jQuery);
