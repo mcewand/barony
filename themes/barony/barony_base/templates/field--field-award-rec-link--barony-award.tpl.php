fd273837ea93e6cd6982453bb880b93419a2ea6d
Dan McEwan <dan.mcewan@gmail.com>
2017-09-12 23:45:15 +0000

    updating awards page display

---
+++ themes/barony/barony_base/templates/field--field-award-rec-link--barony-award.tpl.php
@@ -0,0 +1,5 @@
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div class="award-rec"<?php print $content_attributes; ?>>
+    <a href="<?php print $items[0]['#markup'] ?>" target="_blank"><img src="/<?php print drupal_get_path('theme', 'barony_base'); ?>/images/recommend-button.png"></a>
+  </div>
+</div>
